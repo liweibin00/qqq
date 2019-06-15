6dbd415163ac3191d9fdfdf29577a99b3bdd70e1
liweibin <382488114@qq.com>
2019-06-15 20:29:02 +0800

    这是push文件，马上上传

--- itheima/src/nn.java
+++ itheima/src/nn.java
@@ -2,11 +2,7 @@ public class nn {
     public static void main(String[] args) {
         System.out.println("rrrrrrrrrrrrrrrrrrr");
         System.out.println("rrrrrrrrrrrrrrrrrrr");
-        System.out.println("rrrrrrrrrrrrrrrrrrr");
-        System.out.println("rrrrrrrrrrrrrrrrrrr");
-        System.out.println("rrrrrrrrrrrrrrrrrrr");
-        System.out.println("rrrrrrrrrrrrrrrrrrr");
-        System.out.println("rrrrrrrrrrrrrrrrrrr");
+        System.out.println("dasdsacacac");
 
     }
 }

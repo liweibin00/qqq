ce0edb03e7eb4f6b44f50767b989e7e0a5d52322
liweibin <382488114@qq.com>
2019-06-15 21:02:12 +0800

    这是push文件，马上上传

---
+++ itheima/src/myfenzhilei.java
@@ -0,0 +1,12 @@
+public class myfenzhilei {
+    public static void main(String[] args) {
+        System.out.println("ddddddddddddd");
+        System.out.println("ddddddddddddd");
+        System.out.println("ddddddddddddd");
+        System.out.println("ddddddddddddd");
+        System.out.println("ddddddddddddd");
+        System.out.println("ddddddddddddd");
+        //反反复复烦烦烦烦烦烦烦烦烦
+
+    }
+}

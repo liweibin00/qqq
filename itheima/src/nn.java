f5ccfec76bb4bbf33147dc997f55b8c08d086fa1
liweibin <382488114@qq.com>
2019-06-15 20:22:56 +0800

    修改

---
+++ itheima/src/nn.java
@@ -0,0 +1,18 @@
+public class nn {
+    public static void main(String[] args) {
+        System.out.println("rrrrrrrrrrrrrrrrrrr");
+        System.out.println("rrrrrrrrrrrrrrrrrrr");
+        System.out.println("rrrrrrrrrrrrrrrrrrr");
+        System.out.println("rrrrrrrrrrrrrrrrrrr");
+        System.out.println("rrrrrrrrrrrrrrrrrrr");
+        System.out.println("rrrrrrrrrrrrrrrrrrr");
+        System.out.println("rrrrrrrrrrrrrrrrrrr");
+        System.out.println("rrrrrrrrrrrrrrrrrrr");
+        System.out.println("rrrrrrrrrrrrrrrrrrr");
+        System.out.println("rrrrrrrrrrrrrrrrrrr");
+        System.out.println("rrrrrrrrrrrrrrrrrrr");
+        System.out.println("rrrrrrrrrrrrrrrrrrr");
+        System.out.println("rrrrrrrrrrrrrrrrrrr");
+        System.out.println("rrrrrrrrrrrrrrrrrrr");
+    }
+}

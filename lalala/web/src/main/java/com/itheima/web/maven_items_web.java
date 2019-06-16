8caf94da452ce0d15650e43053f07694a8a88db6
liweibin <382488114@qq.com>
2019-06-16 16:42:15 +0800

    修改

--- lalala/web/src/main/java/com/itheima/web/maven_items_web.java
+++ lalala/web/src/main/java/com/itheima/web/maven_items_web.java
@@ -4,7 +4,7 @@ import com.itheima.domain.Items;
 import org.springframework.ui.Model;
 
 //klklppp
-//klkl
+//klklpopo
 public interface maven_items_web {
 
     public String findById(Model model);
